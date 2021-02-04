e94f3f3d4c1f05b07da4c482122f418dfe04791f
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-04 15:31:34 -0500

    gave user 5 guesses

--- js/loops.js
+++ js/loops.js
@@ -1,7 +1,7 @@
 /* make the random number*/
 /* store it the random number in value*/
 
-let randomNum = Math.floor((Math.random() * 1) + 100);
+let randomNum = Math.floor((Math.random() * 100) + 1);
 
 /* prompt the user to guess a number*/
 /* store that number in a var*/
@@ -10,7 +10,21 @@ let userNum = prompt("Guess a number between 1 and 100");
 
 /* create a conditional statement to check the random number against the guessed number */
 
-if ( userNum == randomNum) {
+/*if ( userNum == randomNum ) {
     alert("You guessed correctly! Congrats!")
 } else {
-    alert(`You guessed incorrectly, the number was: ${randomNum}. Reload the page and try again!`)
+    alert(`You guessed incorrectly, the number was: ${randomNum}. Reload the page and try again!`)*/
+
+    //Give user 5 attempts to guess number
+
+console.log(randomNum)
+
+for ( let i = 4; i > 0; i-- ) {
+    if ( userNum == randomNum ) {
+        alert(`You guessed correctly! Congrats!`);
+        break
+    } else{
+        alert(`You guessed incorrectly, you have ${i} tries left to get it right.`);
+        userNum = prompt("Guess a number between 1 and 100");
+    }
+}
