1a9f0acbf924fee23c9c13858ce1339aa4069df3
Your Name <you@example.com>
2021-02-05 11:01:42 -0500

    added triangle files

---
+++ js/triangle.js
@@ -0,0 +1,20 @@
+console.log("connected")
+/* ask the user to enter a value*/
+/* validate if the input is a number*/
+/* if value isn't a number ask user to enter a number*/
+
+let userNum = prompt("Give me a number")
+while(isNaN(userNum)){
+    userNum = prompt("Give me a number")
+}
+
+// count from 1 to user's input
+// print to webpage a line of characters matching the current count
+
+for(let i=1; i<userNum; i++){
+   while(i<userNum){
+    document.getElementById("numbers").innerHTML = "* <br>"
+   }
+}
+
+
